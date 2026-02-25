4df90d2078e9b819cc2e8ce602beee4e8a2e58b3
Seu Nome <seuemail@exemplo.com>
2026-02-24 21:56:50 -0300

    ﻿fix(machine_routes): corrigir header quebrado que gerava SyntaxError no VS Code
    - Linha LAST_RECODE ficou sem comentario e causava erro de sintaxe

--- modules/machine_routes.py
+++ modules/machine_routes.py
@@ -3027,4 +3027,4 @@ def historico_producao_api():
 # - Mantem logica de producao_evento e producao_diaria
 # "@ | Set-Content commitmsg.txt -Encoding UTF8
 # git commit -F commitmsg.txt
-# git pu
+# git pufgf
