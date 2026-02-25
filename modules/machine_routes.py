f54b05070aebb0448ce846e5c2867d5b22af391d
Seu Nome <seuemail@exemplo.com>
2026-02-24 21:11:39 -0300

    ﻿fix(machine/config): recarregar config_v2 do SQLite apos deploy
    - Aplica config_v2 persistida (machine_config.config_json) na memoria em:
      /machine/status, /machine/update e /api/producao/historico
    - Garante que a tela de configuracao e calculos usem a config salva mesmo apos restart/deploy

--- modules/machine_routes.py
+++ modules/machine_routes.py
@@ -2993,4 +2993,4 @@ def historico_producao_api():
 # - Mantem logica de producao_evento e producao_diaria
 # "@ | Set-Content commitmsg.txt -Encoding UTF8
 # git commit -F commitmsg.txt
-# git push
+# git pus
